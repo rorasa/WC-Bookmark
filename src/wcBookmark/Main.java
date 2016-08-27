e76fee48c60809a611e88a46e0e076ea18ad7cbe
Wattanit Hotrakool <wattanit.h@gmail.com>
2016-08-27 23:40:26 +0100

    Replace new Main.fxml UI

--- src/wcBookmark/Main.java
+++ src/wcBookmark/Main.java
@@ -13,20 +13,20 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("WC-Bookmark.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
         Parent root = loader.load();
 
-        DbManager db = new DbManager("wcdb");
+//        DbManager db = new DbManager("wcdb");
 
-        Controller controller = loader.getController();
-        controller.setDatabase(db);
+//        Controller controller = loader.getController();
+//        controller.setDatabase(db);
 
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Bookmark Collection | Wattanit's Collection");
         primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             public void handle(WindowEvent we) {
-                db.closeConnection();
+ //               db.closeConnection();
             }
         });
     }
